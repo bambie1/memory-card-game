8a5fc7480c0edeb1ec18bf8334fbfc28e51da631
Benaiah Ambie-Barango <bennieb96@gmail.com>
2020-05-19 12:47:12 -0400

    cra setup

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Card.scss";
+
+class Card extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return <div className="card-component">A card</div>;
+  }
+}
+
+export default Card;
